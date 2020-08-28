efaf970ce93b8d8ab75a459f2e59e1ba53655d0d
shuvocasanova <24757915+shuvocasanova@users.noreply.github.com>
2020-08-29 01:24:49 +0600

    Add files via upload

---
+++ build/precache-manifest.a9aac512241e2b00a98263f3167b2867.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518362508cbbe82653eb7e49bcec4b2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cdf2cd1e35dbd9d73fa",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0ac9c0858ed3e53da5f8",
+    "url": "/static/css/main.967bd44a.chunk.css"
+  },
+  {
+    "revision": "5cdf2cd1e35dbd9d73fa",
+    "url": "/static/js/2.e3d965c9.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.e3d965c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac9c0858ed3e53da5f8",
+    "url": "/static/js/main.aaaf6c06.chunk.js"
+  },
+  {
+    "revision": "c291e32bd78050abef8e",
+    "url": "/static/js/runtime-main.6738ea98.js"
+  },
+  {
+    "revision": "d48db7be5d89e0df0d095e66efe2cbc7",
+    "url": "/static/media/logo.d48db7be.png"
+  }
+]);
